08553be55b7e985f47c89861ea1adaa4aabc608a
Shubham Sharma <shubham@iastate.edu>
2019-09-05 21:19:05 -0500

    Finished lab 1 code

--- Lab01/app/src/main/java/edu/iastate/helloworld/MainActivity.java
+++ Lab01/app/src/main/java/edu/iastate/helloworld/MainActivity.java
@@ -3,12 +3,27 @@ package edu.iastate.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Random mRandom;
+    private String[] mMessages = {"Munster", "Tigger", "Catillac", "Kitty Purry", "Crookshanks", "Sylvester", "Felix", "Cheshire"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRandom = new Random();
+    }
+
+    public void changeText(View view) {
+        TextView textView = (TextView) findViewById(R.id.text);
+        int randInt = mRandom.nextInt(mMessages.length);
+        textView.setText(mMessages[randInt]);
     }
 }
