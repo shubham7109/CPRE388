a72a19d59b5ea432f08364ce11e45d6acd18d50f
Shubham Sharma <shubham@mac-air-047.cs.iastate.edu>
2019-09-19 20:04:35 -0500

    Kinda done

--- Lab02/PocketCalculator_skeleton/app/src/main/java/edu/iastate/netid/pocketcalculator/MainActivity.java
+++ Lab02/PocketCalculator_skeleton/app/src/main/java/edu/iastate/netid/pocketcalculator/MainActivity.java
@@ -3,6 +3,8 @@ package edu.iastate.netid.pocketcalculator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO - uncomment below after layout has been made.
         // Locate the instance of the calculator display TextView.  Don't forget to set the ID in the layout file.
-        //mCalculatorDisplay = findViewById(R.id.CalculatorDisplay);
+        mCalculatorDisplay = findViewById(R.id.CalculatorDisplay);
 
     }
 
@@ -36,6 +38,86 @@ public class MainActivity extends AppCompatActivity {
     public void onEqualClicked(View view) {
         try {
             mCalculationStream.calculateResult();
+        } finally {
+            updateCalculatorDisplay();
+        }
+    }
+
+    public void onDigitClicked(View view) {
+        try {
+
+            String clicked = ((TextView) view).getText().toString();
+
+            switch (clicked){
+                case "1":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.ONE);
+                    break;
+                case "2":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.TWO);
+                    break;
+                case "3":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.THREE);
+                    break;
+                case "4":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.FOUR);
+                    break;
+                case "5":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.FIVE);
+                    break;
+                case "6":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.SIX);
+                    break;
+                case "7":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.SEVEN);
+                    break;
+                case "8":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.EIGHT);
+                    break;
+                case "9":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.NINE);
+                    break;
+                case "0":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.ZERO);
+                    break;
+                case ".":
+                    mCalculationStream.inputDigit(CalculationStream.Digit.DECIMAL);
+                    break;
+
+            }
+        } finally {
+            updateCalculatorDisplay();
+        }
+    }
+
+    public void onOperandClicked(View view){
+
+        try{
+
+            String operand = ((TextView) view).getText().toString();
+
+            switch (operand){
+                case "+":
+                    mCalculationStream.inputOperation(CalculationStream.Operation.ADD);
+                    break;
+                case "/":
+                    mCalculationStream.inputOperation(CalculationStream.Operation.DIVIDE);
+                    break;
+                case "-":
+                    mCalculationStream.inputOperation(CalculationStream.Operation.SUBTRACT);
+                    break;
+                case "*":
+                    mCalculationStream.inputOperation(CalculationStream.Operation.MULTIPLY);
+                    break;
+
+            }
+        } finally {
+            updateCalculatorDisplay();
+        }
+    }
+
+    public void onClearClicked(View view){
+        try{
+            mCalculationStream.clear();
         } finally {
             updateCalculatorDisplay();
         }
